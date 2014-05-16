fb99cc8981293394c3dc9cc33181cfcba8aefe59
Ted Ghose <tghose@contrailsystems.com>
2014-05-16 09:19:37 -0700

    vif to use vrouter-api

--- nova_contrail_vif/contrailvif.py
+++ nova_contrail_vif/contrailvif.py
@@ -1,186 +1,66 @@
-import copy
 import gettext
 
 gettext.install('contrail_vif')
 
-from oslo.config import cfg
-
 from nova import exception
 from nova.network import linux_net
-from nova.network import model as network_model
 from nova.openstack.common import log as logging
 from nova.openstack.common import loopingcall
-from nova import utils
-from nova.virt.libvirt import config as vconfig
 from nova.virt.libvirt import designer
 from nova.virt.libvirt.vif import LibvirtBaseVIFDriver
-from gen_py.instance_service import InstanceService
+from contrail_vrouter_api.vrouter_api import ContrailVRouterApi
 
 LOG = logging.getLogger(__name__)
 
+
 class VRouterVIFDriver(LibvirtBaseVIFDriver):
     """VIF driver for VRouter when running Quantum."""
-    
+
     def __init__(self, get_connection):
         super(VRouterVIFDriver, self).__init__(get_connection)
-        self._agent_alive = False
-        self._agent_connected = False
-        self._port_dict = {}
-        self._protocol = None
-        timer = loopingcall.FixedIntervalLoopingCall(self._keep_alive)
+        self._vrouter = ContrailVRouterApi()
+        timer = loopingcall.FixedIntervalLoopingCall(
+                  self._vrouter.periodic_connection_check)
         timer.start(interval=2)
     #end __init__
 
-    def _agent_connect(self, protocol):
-        # Agent connect for first time
-        if protocol != None:
-            #from instance_service import InstanceService
-	    service = InstanceService.Client(protocol)
-	    return service.Connect()
-        else:
-            return False
-
-    #end __agent_connect
-
-    def _keep_alive(self):
-        try:
-            if self._agent_alive == False:
-                self._protocol = self._agent_conn_open()
-                if self._protocol == None:
-                    return
-
-            #from instance_service import InstanceService
-            service = InstanceService.Client(self._protocol)
-            aa_latest = service.KeepAliveCheck()
-            if self._agent_alive == False and aa_latest == True:
-                port_l = [v for k, v in self._port_dict.iteritems()]
-                LOG.debug(_('Agent sending port list %d, %s'), len(port_l), self)
-                if len(port_l):
-                    for i in range(len(port_l)):
-                        LOG.debug(_('Port %s %s'), port_l[i].tap_name, port_l[i].ip_address)
-
-                service.AddPort(port_l)
-                self._agent_alive = True
-                return
-
-            if self._agent_alive == True and aa_latest == False:
-                LOG.debug(_('Agent not available, %s'), self)
-                self._agent_alive = False
-                return
-
-        except:
-            self._agent_alive = False
-            LOG.debug(_('Agent keep alive exception: %s'), self)
-    #end _keep_alive
-
-    def _agent_conn_open(self):
-        import socket
-        import sys
-        import uuid
-
-        from thrift.transport import TTransport, TSocket
-        from thrift.transport.TTransport import TTransportException
-        from thrift.protocol import TBinaryProtocol, TProtocol
-        #from instance_service import InstanceService
-        from gen_py.instance_service import ttypes
-
-        try:
-            socket = TSocket.TSocket("127.0.0.1", 9090)
-            transport = TTransport.TFramedTransport(socket)
-            transport.open()
-            protocol = TBinaryProtocol.TBinaryProtocol(transport)
-            self._agent_connected = self._agent_connect(protocol)
-            return protocol
-        except TTransportException:
-            return None
-    #end _agent_conn_open
-
-    def _convert_to_bl(self, id):
-        import uuid
-        hexstr = uuid.UUID(id).hex
-        return [int(hexstr[i:i+2], 16) for i in range(32) if i%2 == 0]
-    #end _convert_to_bl
-
-    def _agent_inform(self, port, id, add):
-        # First add to the port list
-        if add == True:
-            self._port_dict[id] = port
-        else:
-            if id in self._port_dict:
-                del self._port_dict[id]
-
-        if not self._agent_alive:
-            return
-
-        #from instance_service import InstanceService
-        LOG.debug(_('agent_inform %s, %s, %s, %s'), 
-                  port.ip_address, 
-                  port.tap_name,
-                  add,
-                  self)
-        import socket
-        try:
-            service = InstanceService.Client(self._protocol)
-            if add == True:
-                service.AddPort([port])
-            else:
-                service.DeletePort(port.port_id)
-        except:
-            self._agent_alive = False
-
-    #end _agent_inform
-
     def get_config(self, instance, vif, image_meta, inst_type):
-        conf = super(VRouterVIFDriver, self).get_config(instance, vif, image_meta, inst_type)
+        conf = super(VRouterVIFDriver, self).get_config(instance, vif,
+                image_meta, inst_type)
         dev = self.get_vif_devname(vif)
         designer.set_vif_host_backend_ethernet_config(conf, dev)
-    
+
         return conf
+    #end get_config
 
     def plug(self, instance, vif):
         iface_id = vif['id']
         dev = self.get_vif_devname(vif)
         linux_net.create_tap_dev(dev)
+        LOG.debug(_('Plug %s[%s]' % (instance['display_name'], iface_id)))
 
-        # port_id(tuuid), instance_id(tuuid), tap_name(string), 
-        # ip_address(string), vn_id(tuuid)
-        import socket
-        from gen_py.instance_service import ttypes
-        port = ttypes.Port(self._convert_to_bl(iface_id), 
-                           self._convert_to_bl(instance['uuid']), 
-                           dev, 
-                           vif['network']['subnets'][0]['ips'][0]['address'],
-                           self._convert_to_bl(vif['network']['id']),
-                           vif['address'],
-	                   instance['display_name'],
-	                   instance['hostname'],
-	                   instance['host'],
-	                   self._convert_to_bl(instance['project_id']))
-
-        self._agent_inform(port, iface_id, True)
+        self._vrouter.add_port(
+          vm_uuid_str=instance['uuid'],
+          vif_uuid_str=iface_id,
+          interface_name=dev,
+          mac_address=vif['address'],
+          ip_address=vif['network']['subnets'][0]['ips'][0]['address'],
+          network_uuid=vif['network']['id'],
+          display_name=instance['display_name'],
+          hostname=instance['host'],
+          vm_project_uuid=instance['project_id']
+                )
     #end plug
 
     def unplug(self, instance, vif):
         """Unplug the VIF from the network by deleting the port from
         the bridge."""
-        LOG.debug(_('Unplug'))
         iface_id = vif['id']
         dev = self.get_vif_devname(vif)
+        LOG.debug(_('Unplug %s[%s]' % (instance['display_name'], iface_id)))
 
-        import socket
-        from gen_py.instance_service import ttypes
-        port = ttypes.Port(self._convert_to_bl(iface_id), 
-                           self._convert_to_bl(instance['uuid']), 
-                           dev, 
-                           vif['network']['subnets'][0]['ips'][0]['address'],
-                           self._convert_to_bl(vif['network']['id']),
-                           vif['address'],
-	                   instance['display_name'],
-	                   instance['hostname'],
-	                   instance['host'],
-	                   self._convert_to_bl(instance['project_id']))
+        self._vrouter.delete_port(iface_id)
 
-        self._agent_inform(port, iface_id, False)
         linux_net.delete_net_dev(dev)
 
     #end unplug
--- setup.py
+++ setup.py
@@ -8,8 +8,6 @@ setup(
     name='nova_contrail_vif',
     version='0.1dev',
     packages=['nova_contrail_vif',
-              'nova_contrail_vif.gen_py',
-              'nova_contrail_vif.gen_py.instance_service',
               ],
     package_data={'': ['*.html', '*.css', '*.xml']},
     zip_safe=False,
