16162e562790e0017f5ac94e34f446cba786072e
Michal Dubiel <md@semihalf.com>
2014-11-27 11:34:41 +0100

    Initial support for vhost-user feature of QEMU
    Part of this is a workaround. Proper way of doing that would be to set
    the correct VIF type in Neutron plugin and then, depending on the type use
    either ethernet backed with TAPs or vhost-user option.
    
    Change-Id: I315e6429b5a5d85f48f46bc0aaec8bd2cc39e468

--- nova_contrail_vif/contrailvif.py
+++ nova_contrail_vif/contrailvif.py
@@ -18,6 +18,7 @@
 import gettext
 import threading
 import time
+import os.path as path
 
 gettext.install('contrail_vif')
 
@@ -60,6 +61,20 @@ from nova.compute import utils as compute_utils
 orig_get_nw_info_for_instance = None
 compute_mgr = None
 
+CONF = cfg.CONF
+contrail_vif_opts = {
+    cfg.BoolOpt('use_userspace_vhost',
+                default=False,
+                help='Use qemu userspace-vhost for backing guest interfaces'),
+    cfg.StrOpt('userspace_vhost_socket_dir',
+               default='/var/tmp',
+               help='Directory for userspace vhost sockets'),
+}
+CONF.register_opts(contrail_vif_opts, 'contrail')
+CONF.import_opt('number_of_virtio_queues', 'nova.virt.libvirt.vif',
+                group='libvirt')
+
+
 # MonkeyPatch the vif_driver with VRouterVIFDriver during restart of nova-compute
 def patched_get_nw_info_for_instance(instance):
     if not isinstance(compute_mgr.driver.vif_driver, VRouterVIFDriver):
@@ -69,7 +84,7 @@ def patched_get_nw_info_for_instance(instance):
 
 class ContrailNetworkAPI(API):
     def __init__(self):
-        # MonkeyPatch the compute_utils.get_nw_info_for_instance with 
+        # MonkeyPatch the compute_utils.get_nw_info_for_instance with
         # patched_get_nw_info_for_instance to enable overwriting vif_driver
         if orig_get_nw_info_for_instance is None:
             global orig_get_nw_info_for_instance
@@ -105,7 +120,7 @@ class ContrailNetworkAPI(API):
 
 class VRouterVIFDriver(LibVirtVIFDriver):
     """VIF driver for VRouter when running Neutron."""
-    
+
     PORT_TYPE = 'NovaVMPort'
 
     def __init__(self, get_connection):
@@ -139,10 +154,10 @@ class VRouterVIFDriver(LibVirtVIFDriver):
 
     def get_config(self, instance, vif, image_meta, inst_type, virt_type=None):
         try:
-            conf = super(VRouterVIFDriver, self).get_config(instance, vif, 
+            conf = super(VRouterVIFDriver, self).get_config(instance, vif,
                                                         image_meta, inst_type)
         except TypeError:
-            conf = super(VRouterVIFDriver, self).get_base_config(instance, vif, 
+            conf = super(VRouterVIFDriver, self).get_base_config(instance, vif,
                                              image_meta, inst_type, virt_type)
         dev = self.get_vif_devname(vif)
         if not virt_type:
@@ -156,7 +171,13 @@ class VRouterVIFDriver(LibVirtVIFDriver):
             br_name = self._get_br_name(dev)
             designer.set_vif_host_backend_bridge_config(conf, br_name)
         else:
-            designer.set_vif_host_backend_ethernet_config(conf, dev)
+            if CONF.contrail.use_userspace_vhost:
+                dev = path.join(CONF.contrail.userspace_vhost_socket_dir,
+                                'uvh_vif_' + dev)
+                designer.set_vif_host_backend_vhostuser_config(conf, 'client',
+                        dev)
+            else:
+                designer.set_vif_host_backend_ethernet_config(conf, dev)
         designer.set_vif_bandwidth_config(conf, inst_type)
 
         return conf
@@ -165,7 +186,8 @@ class VRouterVIFDriver(LibVirtVIFDriver):
         dev = self.get_vif_devname(vif)
 
         try:
-            linux_net.create_tap_dev(dev)
+            if not CONF.contrail.use_userspace_vhost:
+                linux_net.create_tap_dev(dev)
         except processutils.ProcessExecutionError:
             LOG.exception(_LE("Failed while plugging vif"), instance=instance)
 
@@ -237,5 +259,5 @@ class VRouterVIFDriver(LibVirtVIFDriver):
         if virt_type == 'lxc':
             linux_net.LinuxBridgeInterfaceDriver.remove_bridge(
                     self._get_br_name(dev))
-        linux_net.delete_net_dev(dev)
-
+        if not CONF.contrail.use_userspace_vhost:
+            linux_net.delete_net_dev(dev)
