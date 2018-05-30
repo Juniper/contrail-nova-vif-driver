4802cb06930a04b20feb77d6c66b9d57a0e9d578
Jan Gutter <jan.gutter@netronome.com>
2018-05-30 13:08:32 +0200

    Refactor call to vrouter-port-control
    This changeset breaks the vrouter-port-control call out into
    discrete functions. This is a cleanup, preserving existing
    test expectations, to allow for common code to be used with multiple
    plugging modes.
    
    The unit tests have been updated to reflect that the calls to the
    vrouter-port-control script are now discrete parameters rather than
    a single string.
    
    Change-Id: Ia554b5d1e3036eff06762dd4e8fe71a9e6715a35
    Partial-Bug: #1773981
    Signed-off-by: Jan Gutter <jan.gutter@netronome.com>

--- vif_plug_vrouter/vrouter.py
+++ vif_plug_vrouter/vrouter.py
@@ -27,23 +27,115 @@ from vif_plug_vrouter.i18n import _LE
 from vif_plug_vrouter import privsep
 
 LOG = logging.getLogger(__name__)
+VHOSTUSER_MODE_SERVER = 1
+VHOSTUSER_MODE_CLIENT = 0
 
 
 @privsep.vif_plug.entrypoint
-def run_vrouter_port_control(args):
+def plug_contrail_vif(vif_id, vm_id, net_id, project_id, ip_addr, ip6_addr,
+                      vm_name, mac, dev_name, port_type, vif_type=None,
+                      vnic_type=None, pci_dev=None, vhostuser_socket=None,
+                      vhostuser_mode=None):
+    """Call the vrouter port control script to plug a VIF
+
+    :param vif_id: VIF ID to plug
+    :param vm_id: Instance ID
+    :param net_id: Network ID
+    :param project_id: Project ID associated with the instance
+    :param ip_addr: IPv4 address to assign to the interface
+    :param ip6_addr: IPv6 address to assign to the interface
+    :param vm_name: Display name of the instance
+    :param mac: MAC address to assign to the interface
+    :param dev_name: Name of the TAP/device to plug
+    :param port_type: vrouter port type (e.g. NovaVMPort)
+    :param vif_type: vrouter VIF type (e.g. VhostUser)
+    :param vnic_type: Selector for offload mode (e.g. direct)
+    :param pci_dev: Virtual Function to assign for offloading
+    :param vhostuser_socket: vhost-user socket path
+    :param vhostuser_mode: vhost-user mode (client/server)
+    """
+    cmd = (
+        'vrouter-port-control',
+        '--oper=add',
+        '--uuid=%s' % vif_id,
+        '--instance_uuid=%s' % vm_id,
+        '--vn_uuid=%s' % net_id,
+        '--vm_project_uuid=%s' % project_id,
+        '--ip_address=%s' % ip_addr,
+        '--ipv6_address=%s' % ip6_addr,
+        '--vm_name=%s' % vm_name,
+        '--mac=%s' % mac,
+        '--tap_name=%s' % dev_name,
+        '--port_type=%s' % port_type,
+    )
+    if vif_type:
+        cmd += ('--vif_type=%s' % vif_type,)
+    if vnic_type:
+        cmd += ('--vnic_type=%s' % vnic_type,)
+    if pci_dev:
+        cmd += ('--pci_dev=%s' % pci_dev,)
+    if vhostuser_socket:
+        cmd += ('--vhostuser_socket=%s' % vhostuser_socket,)
+    if vhostuser_mode is not None:
+        cmd += ('--vhostuser_mode=%s' % vhostuser_mode,)
+    cmd += (
+        '--tx_vlan_id=%d' % -1,
+        '--rx_vlan_id=%d' % -1,
+    )
     try:
-        processutils.execute("vrouter-port-control", args)
+        processutils.execute(*cmd)
     except Exception as e:
-        LOG.error(_LE("Unable to execute vrouter-port-control "
+        LOG.error(_LE("Unable to execute vrouter-port-control " +
                       "%(args)s.  Exception: %(exception)s"),
-                  {'args': args, 'exception': e})
-        raise exception.VrouterPortControlError(args=args)
+                  {'args': cmd, 'exception': e})
+        raise exception.VrouterPortControlError(args=cmd)
+
+
+@privsep.vif_plug.entrypoint
+def unplug_contrail_vif(port_id, pci_dev=None, vnic_type=None,
+                        vhostuser_mode=None, vhostuser_socket=None):
+    """Call the vrouter port control script to unplug a vif
+
+    :param port_id: VIF ID to unplug
+    :param vnic_type: Selector for offload mode (e.g. direct)
+    :param pci_dev: Virtual Function to assign for offloading
+    :param vhostuser_socket: vhost-user socket path
+    :param vhostuser_mode: vhost-user mode (client/server)
+    """
+    cmd = (
+        'vrouter-port-control',
+        '--oper=delete',
+        '--uuid=%s' % port_id,
+    )
+    if vnic_type:
+        cmd += ('--vnic_type=%s' % vnic_type,)
+    if pci_dev:
+        cmd += ('--pci_dev=%s' % pci_dev,)
+    if vhostuser_socket:
+        cmd += ('--vhostuser_socket=%s' % vhostuser_socket,)
+    if vhostuser_mode:
+        cmd += ('--vhostuser_mode=%s' % vhostuser_mode,)
+    try:
+        processutils.execute(*cmd)
+    except Exception as e:
+        LOG.error(_LE("Unable to execute vrouter-port-control " +
+                      "%(args)s.  Exception: %(exception)s"),
+                  {'args': cmd, 'exception': e})
+        raise exception.VrouterPortControlError(args=cmd)
 
 
 class VrouterPlugin(plugin.PluginBase):
-    """A vRouter plugin that can setup VIFs in both kernel and vhostuser mode.
+    """An os-vif vRouter plugin that can setup VIFs in both kernel and
+       dpdk vhostuser mode.
 
-    TODO: Add more detailed description.
+    This is the unified os-vif plugin for the following OS-VIF plugging modes:
+
+      * DPDK vhost-user plugging (VIFVHostUser)
+      * TBD: Classic kernel plugging (vrouter.ko) via TAP device (VIFGeneric)
+
+    This plugin gets called by Nova to plug the VIFs into and unplug them from
+    the datapath. There is corresponding code in Nova to configure the
+    hypervisor configure the VM to connect to the required connection method.
     """
 
     def describe(self):
@@ -87,27 +179,24 @@ class VrouterPlugin(plugin.PluginBase):
             ptype = 'NameSpacePort'
 
         vif_type = 'Vrouter'
-        vhostuser_socket = ''
-        vhostuser_mode = ''
+        vhostuser_socket = None
+        vhostuser_mode = None
+        vnic_type = None
+        pci_dev = None
+
         if isinstance(vif, objects.vif.VIFVHostUser):
             vif_type = 'VhostUser'
-            vhostuser_socket = ' --vhostuser_socket=%s' % vif.path
+            vhostuser_socket = vif.path
             if vif.mode == 'server':
-                vhostuser_mode = ' --vhostuser_mode=1'
+                vhostuser_mode = VHOSTUSER_MODE_SERVER
             else:
-                vhostuser_mode = ' --vhostuser_mode=0'
+                vhostuser_mode = VHOSTUSER_MODE_CLIENT
 
-        cmd_args = ("--oper=add --uuid=%s --instance_uuid=%s --vn_uuid=%s "
-                    "--vm_project_uuid=%s --ip_address=%s --ipv6_address=%s"
-                    " --vm_name=%s --mac=%s --tap_name=%s --port_type=%s "
-                    "--vif_type=%s%s%s --tx_vlan_id=%d --rx_vlan_id=%d" %
-                    (vif.id, instance_info.uuid, vif.network.id,
-                     instance_info.project_id, ip_addr, ip6_addr,
-                     instance_info.name, vif.address,
-                     vif.vif_name, ptype, vif_type, vhostuser_socket,
-                     vhostuser_mode, -1, -1))
-
-        run_vrouter_port_control(cmd_args)
+        plug_contrail_vif(vif.id, instance_info.uuid, vif.network.id,
+                          instance_info.project_id, ip_addr, ip6_addr,
+                          instance_info.name, vif.address,
+                          vif.vif_name, ptype, vif_type, vnic_type, pci_dev,
+                          vhostuser_socket, vhostuser_mode)
 
     def plug(self, vif, instance_info):
         if not (isinstance(vif, objects.vif.VIFVHostUser) or
@@ -118,8 +207,7 @@ class VrouterPlugin(plugin.PluginBase):
 
     @staticmethod
     def _vrouter_port_delete(instance_info, vif):
-        cmd_args = ("--oper=delete --uuid=%s" % (vif.id))
-        run_vrouter_port_control(cmd_args)
+        unplug_contrail_vif(vif.id)
 
     def unplug(self, vif, instance_info):
         if not (isinstance(vif, objects.vif.VIFVHostUser) or
